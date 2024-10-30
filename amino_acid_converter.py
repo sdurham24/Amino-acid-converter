062c39e45dd11717dc6af5f567a2cfaeea7221f3
Sophia Durham <154894263+sdurham24@users.noreply.github.com>
2024-10-30 14:36:57 +0000

    More accurate function naming added.

--- amino_acid_converter.py
+++ amino_acid_converter.py
@@ -1,9 +1,9 @@
 amino_acid_dict = {"ALA":"A", "ARG":"R", "ASP":"D", "ASN":"N","CYS":"C", "GLU":"E", "GLN":"Q", "GLY":"G", "HIS":"H", "ILE":"I", "LEU":"L", "LYS":"K", "MET":"M", "PHE":"F", "PRO":"P", "SER":"S", "THR":"T", "TRP":"W", "TYR":"Y", "VAL":"V"}
 
-converter = input("Which converter do you want to use?\nFor three letter to one letter, type '321'\nFor one letter to three letter, type '123': ")
+converter = input("Which converter do you want to use?\nFor three letters to one letter, type '321'\nFor one letter to three letters, type '123': ")
 
 
-def convert_to_letters():
+def convert_to_one():
   peptide = input("Enter amino acid sequence in the form of 'Gly-Arg-...' etc: ")
   
   peptide_list = peptide.upper().split("-")
@@ -15,7 +15,7 @@ def convert_to_letters():
 
 
 
-def convert_from_letters():
+def convert_to_three():
   new_amino_acid_dict = dict(zip(amino_acid_dict.values(),amino_acid_dict.keys()))
   #print(new_amino_acid_dict)
   peptide = input("Enter amino acid sequence in the form of 'G,A,P...' etc: ")
@@ -27,9 +27,9 @@ def convert_from_letters():
   return print(new_peptide_string.title())
   
 if converter == '321':
-  convert_to_letters()
+  convert_to_one()
 elif converter == '123':
-  convert_from_letters()
+  convert_to_three()
 else:
   print("Please type a valid response.")
 
